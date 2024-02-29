383b8a297a9083fa9e8c7550eb7fbc304a5e5b8a
Virus Noir <67117684+VirusNoirrr@users.noreply.github.com>
2024-02-29 13:23:04 +0100

    Create solve.py

---
+++ examples/solve.py
@@ -0,0 +1,9 @@
+from procap import ProCap
+apikey = "" # Api key (required)
+url = "" # Link to captcha (required)
+sitekey = "" # Sitekey (required)
+proxy = "" # Proxy (optional)
+userAgent = "" # User agent (optional)
+rqdata = "" # RQData (optional)
+solver = ProCap(apikey) # Replace "api" with your api key !!
+captchaToken = solver.solve(url=url, sitekey=sitekey, proxy=proxy, userAgent=userAgent, rqdata=rqdata))
