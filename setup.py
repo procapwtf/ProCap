0862fb92973357f3b1c5df47102a8d51a5e6349d
Virus Noir <67117684+VirusNoirrr@users.noreply.github.com>
2024-02-29 23:45:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup, find_packages
+VERSION = "0.0.2"
+DESCRIPTION = "ProCap is a captcha solving service."
+LONG_DESCRIPTION = "A api wrapper for ProCap."
 
 setup(
-    name='procap',
-    version='0.1',
+    name="procap",
+    version=VERSION,
+    author="ProCap",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[
-        "requests"
-    ],
+    install_requires=["requests"],
+    keywords=["python", "captcha", "solver", "hcaptcha", "procap", "captcha solver", "hcaptcha solver", "captcha bypass"],
+    classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+]
 )
